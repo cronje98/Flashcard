153c3b84400b868ea180fc0d4187e67916055ab1
cronje98 <110980995+cronje98@users.noreply.github.com>
2025-09-17 20:38:01 +0200

    Add files via upload

---
+++ Flashcard/quiz.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+
+
+
+
+
+# Creation of the routes below [for adding students, IT and maths mark, and home page]
+app = Flask(__name__) #application instance
+
+
+     
+
+app.secret_key = 'supersecretkey'  # Required for session
+
+# questions for now #CONSIDER: Possibly provide list of questions and answer via file which turns into dictionary
+flashcards = [
+    {"question": "What does AI stand for?", "answer": "Artificial Intelligence"},
+    {"question": "What does OOP stand for?", "answer": "Object Oriented Programming"},
+    #{"question": " ", "answer": " "}
+]
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+        # to know: SESSION is a way to temporarily store data per user — like a short-term memory while they’re using your app.
+        #This will be used to keep track of users scores
+    
+    if "index" not in session or session["index"] >= len(flashcards): #checkes if user started quiz and ensures we continue even if session out of range[session will keep counting despit us restarting]
+        session["index"] = 0 # refer to question num
+        session["score"] = 0 # refer to results score that will be tallied toward the end
+
+    current = flashcards[session["index"]]
+    response = "" # kept empty,this well tell user whether answer is correct or not
+
+    if request.method == "POST":
+        answer = request.form["answer"] #CHANGE: How to ignore upper and lower case?
+        correct = current["answer"]
+
+        if answer == correct:
+            session["score"] += 1 #kept score now on 1
+            response = "Correct!"
+        else:
+            response = f"Incorrect! Correct answer: {current['answer']}"
+
+        session["index"] += 1 #updated question number
+
+        
+
+    return render_template("home.html", question=current["question"], response=response)
+
+    #return render_template('home.html')
+
+
+    
+
+
+
+
+
+    
+
+
+if __name__ == '__main__': #will run flas on terminal and open window
+    app.run(debug=True)
