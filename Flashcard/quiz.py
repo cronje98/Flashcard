1284367eab516fcc8180a62932545e931fd468c7
cronje98 <110980995+cronje98@users.noreply.github.com>
2026-05-12 22:02:48 +0200

    Update quiz.py

--- Flashcard/quiz.py
+++ Flashcard/quiz.py
@@ -1,22 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 
 
-
-
-
-# Creation of the routes below [for adding students, IT and maths mark, and home page]
 app = Flask(__name__) #application instance
-
-
-     
-
 app.secret_key = 'supersecretkey'  # Required for session
 
 # questions for now #CONSIDER: Possibly provide list of questions and answer via file which turns into dictionary
+#Below can be updated, more questions and its answers can be added
 flashcards = [
     {"question": "What does AI stand for?", "answer": "Artificial Intelligence"},
     {"question": "What does OOP stand for?", "answer": "Object Oriented Programming"},
-    #{"question": " ", "answer": " "}
+    
 ]
 
 @app.route("/", methods=["GET", "POST"])
@@ -28,20 +21,29 @@ def home():
         session["index"] = 0 # refer to question num
         session["score"] = 0 # refer to results score that will be tallied toward the end
 
+    
     current = flashcards[session["index"]]
     response = "" # kept empty,this well tell user whether answer is correct or not
 
+    
     if request.method == "POST":
+        
+        
+
         answer = request.form["answer"] #CHANGE: How to ignore upper and lower case?
         correct = current["answer"]
 
-        if answer == correct:
+        if answer.upper() == correct.upper():
             session["score"] += 1 #kept score now on 1
             response = "Correct!"
         else:
             response = f"Incorrect! Correct answer: {current['answer']}"
 
         session["index"] += 1 #updated question number
+        #prevent going out of range
+        if session["index"] >= len(flashcards):
+            session["index"] = 0
+        current = flashcards[session["index"]]
 
         
 
